f90993fc019ccb53b6eee77e97b94ad36f1a0b30
Jana Essam <108252869+JanaElnagar@users.noreply.github.com>
2023-12-21 18:17:58 +0200

    Update command.cc
    final edits

--- command.cc
+++ command.cc
@@ -1,11 +1,6 @@
-
 /*
  * CS354: Shell project
  *
- * Template file.
- * You will need to add more code here to execute the command table.
- *
- * NOTE: You are responsible for fixing any bugs this code may have!
  *
  */
 
@@ -16,15 +11,29 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <signal.h>
+#include <string>
 
 #include "command.h"
+#include <fcntl.h>   
+#include <sys/stat.h> 
+#include <sys/types.h>
+
+//part3
+void childIsTerminated()
+{
+    FILE *logFile = fopen("Log.txt","a");
+    time_t current_time = time(NULL);
+    tm *timeformatted = gmtime(&current_time);
+    fprintf(logFile,"Child is terminated at : %d %d %d\n",timeformatted->tm_hour,timeformatted->tm_min,timeformatted->tm_sec);
+    fclose(logFile);
+}
 
 SimpleCommand::SimpleCommand()
 {
 	// Creat available space for 5 arguments
 	_numberOfAvailableArguments = 5;
 	_numberOfArguments = 0;
-	_arguments = (char **) malloc( _numberOfAvailableArguments * sizeof( char * ) );
+	_arguments = (char **) malloc( _numberOfAvailableArguments * sizeof( char * ) ); 
 }
 
 void
@@ -143,15 +152,216 @@ Command::execute()
 		prompt();
 		return;
 	}
-
+	
 	// Print contents of Command data structure
 	print();
+	
+	// Save default input, output, and error because we will
+	// change them during redirection and we will need to restore them
+	// at the end.
+	// The dup() system call creates a copy of a file descriptor.
+	int defaultin = dup( 0 ); // Default file Descriptor for stdin
+	int defaultout = dup( 1 ); // Default file Descriptor for stdout
+	int defaulterr = dup( 2 ); // Default file Descriptor for stderr
+	int fdpipe[2];
+	
+	if ( pipe(fdpipe) == -1) {
+		perror( "pipe error\n");
+		exit( 2 );
+	}
+	
+	int input_fd, output_fd, error_fd;
+	
+	//part3
+	for(int i = 0; i < _numberOfSimpleCommands; i++)
+    	{
+		int n;
+		char *command = _simpleCommands[i]->_arguments[0];
+		
+		if(!strcasecmp(command,"exit"))
+		{
+		    printf("Good Bye!!\n");
+		    exit(1);
+		}
 
-	// Add execution here
+		if(!strcasecmp(command,"cd"))
+		{
+		    if(_simpleCommands[i]->_numberOfArguments > 1)
+		    {
+			std::string path = _simpleCommands[i]->_arguments[1];
+			n = path.length();
+			char pathArray[n+1];
+			strcpy(pathArray, path.c_str());
+			if(chdir(pathArray)) //on success, zero is returned
+			{
+			    printf("Path not found.\n");
+			}
+			break; // do not continue fork loop
+		    }
+		    else   // home
+		    {
+			//chdir("/mnt/");
+			chdir(getenv("HOME"));
+			break;
+		    }
+		}
+		     
+          
+        if( i == 0 ) // parent, first command
+        {
+		// Input Redirection
+		if (_inputFile)
+		{
+		    input_fd = open(_inputFile, O_RDONLY);
+		    if (input_fd < 0)
+		    {
+		        perror("open input file");
+		        exit(EXIT_FAILURE);
+		    }
+		    dup2(input_fd, 0);
+		    close(input_fd);
+		}
+		else  // no input file specified, use default
+		 {
+		    	dup2( defaultin, 0 );
+		 }
+				         
+		// Error Redirection
+		if (_errFile)
+		{
+		    error_fd = open(_errFile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		    if (error_fd < 0)
+		    {
+		        perror("open error file");
+		        exit(EXIT_FAILURE);
+		    }
+		    dup2(error_fd, 2);
+		    close(error_fd);
+		}
+		else  // no output file specified, use default
+		 {
+		 	error_fd = dup(defaulterr);
+		 }
+         
+         }
+         else if ( i != 0 ) // child process
+         {
+         	dup2(fdpipe[0],0); // read input from previous pipe
+         	close(fdpipe[0]);
+         	close(fdpipe[1]);
+         	if(pipe(fdpipe)==-1)
+            {
+                perror("piping error");
+                exit(2);
+            }
+         }
+ 
+	if(i == _numberOfSimpleCommands-1)  // last simpleCommand, no more pipes
+	{
+		if (_outFile)
+		{
+
+		    if (_append)
+		    {
+		        output_fd = open(_outFile, O_WRONLY | O_CREAT | O_APPEND, 0666);
+		    }
+		    else
+		    {
+		        output_fd = creat(_outFile, 0666);
+		    }
+		    if (output_fd < 0)
+		    {
+		        perror("open output file");
+		        exit(EXIT_FAILURE);
+		    }
+		     dup2(output_fd, 1);
+		     //close(output_fd);  // may be unnecessary
+		}
+		else{
+		    	dup2(defaultout, 1);
+		    	//close( defaultout ); // may be unnecessary
+
+		    }
+	}
+	else if(i !=_numberOfSimpleCommands-1)  // not the last simpleCommand, pipes after it
+	{
+		dup2(fdpipe[1],1); // redirect output to pipe
+		//close(fdpipe[1]);  
+		//close(fdpipe[0]);
+	}
+         
+         
 	// For every simple command fork a new process
-	// Setup i/o redirection
-	// and call exec
 
+	    	/* fork a child process */
+    		int pid = fork();
+    		
+	    	if (pid < 0) { /* error occurred */
+			fprintf(stderr, "Fork Failed");
+			exit(2);
+	   	 }
+	    	else if (pid == 0) { /* child process */
+	    
+	    		
+	    		// close file descriptors that are not needed
+			close(fdpipe[0]);
+			close(fdpipe[1]);
+			close(input_fd);
+			close(output_fd);
+			close(error_fd);
+			childIsTerminated();
+			close( defaultin );
+			close( defaultout );
+			close( defaulterr );    		
+		
+			// and call exec
+			execvp(command, _simpleCommands[i]->_arguments);
+			
+			// exec() is not suppose to return, something went wrong
+			perror( "Error");
+			exit( 3 );
+	    	}
+	    	else{  // parent process
+		    	
+		    	// Restore input, output, and error
+
+			dup2( defaultin, 0 );
+			dup2( defaultout, 1 );
+			dup2( defaulterr, 2 );
+		    	
+		    	if (! _background) { /* parent process */
+				
+				/* parent will wait for the child to complete */
+				waitpid( pid,0,0);
+		 	}
+		 	
+		 	
+		
+	 	}
+	} 
+
+
+	// Close file descriptors that are not needed
+	//close(fdpipe[0]);
+	//close(fdpipe[1]);
+	
+	if(_inputFile){
+		close(input_fd);
+	}
+	if(_outFile){
+		close(output_fd);
+	}
+	if(_errFile){
+		close(error_fd);
+	}
+	//close(output_fd);
+	//close(error_fd);
+	
+	close( defaultin );
+	close( defaultout );
+	close( defaulterr );
+
+	
 	// Clear to prepare for next command
 	clear();
 	
@@ -173,9 +383,18 @@ SimpleCommand * Command::_currentSimpleCommand;
 
 int yyparse(void);
 
-int 
+//part3
+void ignoreCtrlC(int Signal)
+{
+    printf("\nTo exit type exit\n");
+    Command::_currentCommand.prompt();
+    
+}
+int
 main()
 {
+    //part3
+    signal(SIGINT,ignoreCtrlC);
 	Command::_currentCommand.prompt();
 	yyparse();
 	return 0;
